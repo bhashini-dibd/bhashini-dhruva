82e4568543e3eec694d1c6f521a3aff5522abff8
rathan-muralidhar <rathan.muralidhar@gmail.com>
2024-01-01 15:17:45 +0530

    Added profanityFilter to ASR and TTS inference Requent Config

--- server/module/services/utilities/profanity/profanity_filter.py
+++ server/module/services/utilities/profanity/profanity_filter.py
@@ -5,12 +5,17 @@ profanity.load_censor_words()
 
 class ProfanityFilter():
     def english_censor(self, text):
-        return profanity.censor(text)
+        return profanity.censor(text)   
     
+    def hindi_censor(self, text):
+        return text
+
     def censor_words(self, language, text):
         try:
             if language == "en":
                 return self.english_censor(text)
+            if language == "hi":
+                return self.hindi_censor(text)
             else:
                 return text
         except:
